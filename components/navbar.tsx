ab42053319ca470ca729527d7a894e5d925cdc97
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-05-12 22:42:32 +0530

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import {MainNav} from "@/components/main-nav"
 import StoreSwitcher from "@/components/store-switcher"
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Navbar = async () =>  {
 
@@ -19,11 +20,12 @@ const Navbar = async () =>  {
   })
 
   return (
-    <div className="border-b">
+    <div className="border-b ">
      <div className="flex h-16 items-center px-4">
       <StoreSwitcher items={stores} />    
       <MainNav className="mx-6"/>
       <div className="ml-auto flex items-center space-x-4">
+        <ThemeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
      </div> 

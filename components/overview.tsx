a567afe40db3277fe896dace59ed14157b773192
Yatik1 <yatiksrivastava1@gmail.com>
2025-07-23 13:16:02 +0530

    updating

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,35 +1,65 @@
 "use client";
 
-import {ResponsiveContainer , Bar,BarChart, XAxis, YAxis} from "recharts"
+import { useTheme } from "next-themes";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Area,
+  Tooltip,
+} from "recharts";
+
 
 interface OverviewProps {
-    data:any[]
+  data: {
+    name: string;
+    total: number;
+  }[];
 }
 
-const Overview : React.FC<OverviewProps> = ({
-    data
-}) => {
+const Overview: React.FC<OverviewProps> = ({ data }) => {
+  const { theme } = useTheme()
+
+  let isDark = theme === "dark"
   return (
     <ResponsiveContainer width="100%" height={350}>
-       <BarChart data={data}>
-          <XAxis 
-            dataKey="name"
-            stroke="#888888"
-            fontSize={12}
-            tickLine={false}
-            axisLine={false}
-          />
-          <YAxis 
-            stroke="#888888"
-            fontSize={12}
-            tickLine={false}
-            axisLine={false}
-            tickFormatter={(value) => `${value}`}
-          />
-          <Bar dataKey="total" fill="#3498db" radius={[4,4,0,0]} /> 
-       </BarChart>
+      <AreaChart 
+        accessibilityLayer
+        data={data} 
+        margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}>
+        <XAxis
+          dataKey="name"
+          stroke="#000000"
+          fontSize={10}
+          tickLine={false}
+          axisLine={false}
+        />
+        <Tooltip contentStyle={{height:70, minWidth:"fit-content", width:100, backgroundColor:isDark ? "#020817" : "white",color:isDark ? "white" : "black", border:"0.2px solid #1E293B", borderRadius:"10px", gap:1}}/>
+        <YAxis
+          stroke="#000000"
+          fontSize={10}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `${value}`}
+        />
+        <Area
+          type="monotone"
+          dataKey="total"
+          stroke="#3498db"
+          fill="#3498db"
+          strokeWidth={2}
+          dot={false}
+        />
+      </AreaChart>
     </ResponsiveContainer>
-  )
-}
+  );
+};
 
-export default Overview
+export default Overview;
